f2663e759cc846835374841c9d12c87a0c6dbc72
satria mangasi junior <satriamangasijunior@gmail.com>
2024-05-08 14:03:03 +0700

    final

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    console.log(`Name: ${name}, Email: ${email}`);
+    
+});
+
+
