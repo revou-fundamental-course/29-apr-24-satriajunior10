40e5773a171f3b23b83653d39eb5e9283c06b2d4
satria mangasi junior <satriamangasijunior@gmail.com>
2024-05-08 20:36:34 +0700

    Final perubahan

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,22 @@
-const form = document.getElementById('contact-form');
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Menghentikan pengiriman formulir secara default
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    console.log(`Name: ${name}, Email: ${email}`);
-    
+        // Panggil fungsi validasi saat formulir disubmit
+        validation();
+    });
 });
 
+function validation() {
+    let name = document.getElementById('name').value;   
+    let email = document.getElementById('email').value;   
+    let question = document.getElementById('question').value;   
 
+    // Validasi input
+    if (name === '' || name === null || email === '' || email === null || question === '' || question === null) {
+        alert("Please fill in all fields.");
+    } else {
+        // Redirect ke halaman awal jika formulir tervalidasi
+        window.location.href = "index.html"; // Ganti "index.html" dengan halaman awal Anda
+    }
+}
